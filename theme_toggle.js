2cff690e26368b5084a8d0cc60522e3e1f2fdd14
flowerstranslations <flowerstl.studio@gmail.com>
2025-03-31 03:43:04 -0700

    Add files via upload

--- theme_toggle.js
+++ theme_toggle.js
@@ -1,5 +1,6 @@
 const toggleButton = document.getElementById('theme-toggle');
 toggleButton.addEventListener('click', function(){
+    console.log('button clicked faggot');
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 });
