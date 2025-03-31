f39d0a67e8fd42c188b756888f09021fe38c15cd
flowerstranslations <flowerstl.studio@gmail.com>
2025-03-31 04:28:19 -0700

    Add files via upload

--- theme_toggle.js
+++ theme_toggle.js
@@ -1,8 +1,6 @@
 const toggleButton = document.getElementById('theme-toggle');
 toggleButton.addEventListener('click', function(){
-    console.log('button clicked faggot');
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+    console.log('button clicked');
     console.log("Current classes:", document.body.classList);
     if(document.body.classList.contains('dark-theme')){
         document.body.classList.remove('dark-theme');
