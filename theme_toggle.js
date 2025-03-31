ad8d65f73515dbc2596917147a5cbd581fd92250
flowerstranslations <flowerstl.studio@gmail.com>
2025-03-31 04:16:52 -0700

    Add files via upload

--- theme_toggle.js
+++ theme_toggle.js
@@ -4,4 +4,12 @@ toggleButton.addEventListener('click', function(){
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
     console.log("Current classes:", document.body.classList);
+    if(document.body.classList.contains('dark-theme')){
+        document.body.classList.remove('dark-theme ');
+        document.body.classList.add('light-theme');
+    } else{
+        document.body.classList.add('dark-theme');
+        document.body.classList.remove('light-theme');
+    }
+    console.log('current classes:', document.body.classList);
 });
