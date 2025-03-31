455e35fa44b38ce0e470c84c79771d95da018280
flowerstranslations <flowerstl.studio@gmail.com>
2025-03-31 14:13:29 -0700

    Add files via upload

--- theme_toggle.js
+++ theme_toggle.js
@@ -1,13 +1,19 @@
 const toggleButton = document.getElementById('theme-toggle');
+const sunImage = document.getElementById('sun');
+const moonImage = document.getElementById('moon');
 toggleButton.addEventListener('click', function(){
-    console.log('button clicked');
-    console.log("Current classes:", document.body.classList);
     if(document.body.classList.contains('dark-theme')){
         document.body.classList.remove('dark-theme');
         document.body.classList.add('light-theme');
+        sunImage.style.visibility = "hidden";
+        moonImage.style.visibility = "visible";
     } else{
         document.body.classList.add('dark-theme');
         document.body.classList.remove('light-theme');
+        sunImage.style.visibility = "visible";
+        moonImage.style.visibility = "hidden";
     }
     console.log('current classes:', document.body.classList);
+    console.log('sun visibility:', window.getComputedStyle(sunImage).visibility);
+    console.log('moon visibility:', window.getComputedStyle(moonImage).visibility);
 });
